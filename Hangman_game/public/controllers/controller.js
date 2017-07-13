960b0067ca87c1a312634eb91bc66b15a003d4c1
JaceGarcia <jaces.place@gmail.com>
2017-07-13 13:31:34 -0400

    Add files via upload

--- Hangman_game/public/controllers/controller.js
+++ Hangman_game/public/controllers/controller.js
@@ -1,6 +1,8 @@
+$(function(){
 $("#button1").click(function(){
     alert("this is the alert button");
 });
+})
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];{
 
@@ -11,27 +13,30 @@ for(var i = 0; i < alphabet.length; i++)
 $('#keyboardLetters').append('<div class="btn btn-success Abuttons ">'+alphabet[i]+'</div>');
 }
 
-var words = ['Jace', 'Richared', 'David'];
+var words = ['jace', 'richared', 'david'];
 var randomNumber = Math.floor(words.length * Math.random());
 var randomWords = words[randomNumber].split('');
-for(var i = 0; i < randomWords.length; i++)
-$('').append('<div class="">'+randomWords[i]+'</div>');
-
+for(var i = 0; i < randomWords.length; i++){}
 var spaces = []
     for( var i = 0; i < randomWords.length; i++){
     spaces.push('_ ');
 }
 $('#wordPlayed').append(spaces);
 var lettersSelected;
-var letterMatched = function (){
+function letterMatched (){
     for(var i = 0; i < randomWords.length; i++){
         if(lettersSelected === randomWords[i]){
             spaces[i] = lettersSelected;
+            $('#wordPlayed').text(spaces.join(' '));
+
+
 }}};
 
-$('#wordPlayed').text(spaces.join(' '));
 
     $('.Abuttons').on('click', function(event){
         lettersSelected = $(event.currentTarget).text();
             letterMatched();
+
+
+console.log('.Abuttons')
         });
