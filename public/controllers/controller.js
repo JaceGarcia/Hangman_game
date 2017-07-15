a8ee82e182ea7897b3dcb35eaa8e3d7470d7b06b
JaceGarcia <jaces.place@gmail.com>
2017-07-15 13:11:29 -0400

    Saving public folder and files inside

---
+++ public/controllers/controller.js
@@ -0,0 +1,81 @@
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var words = ['afghanistan', 'albania', 'brazil', 'botswana', 'canada', 'cuba', 'denmark', 'dominica', 'egypt', 'ethiopia', 'fiji', 'finland', 'france', 'gabon', 'greece', 'hati', 'honduras', 'india', 'iran', 'japan', 'jordan', 'kenya', 'kuuait', 'libya'];
+var randomNumber = Math.floor(words.length * Math.random());
+var randomWord = words[randomNumber];
+var arrRandomWord = randomWord.split('');
+var triesLeft = 6;
+var letterSelected;
+var i;
+var spaces = [];
+var canvas= $('canvas');
+var c= canvas.getcontext('2d');
+
+function init() {
+        for(i = 0; i < arrRandomWord.length; i++){
+            spaces.push('_ ');
+        }
+
+    $('#wordPlayed').append(spaces);
+
+    $('#tries').html('Tries Left '+ triesLeft);
+
+    for(i = 0; i < alphabet.length; i++) {
+        $('#keyboardLetters').append('<div class="btn btn-success letters ">'+alphabet[i]+'</div>');
+    }
+
+    createListeners();
+}
+
+function createListeners(){
+    $('.letters').on('click', function(event){
+        letterSelected = $(event.currentTarget).text();
+        letterMatched();
+        disableLetter(event);
+        decrementTries();
+    });
+
+    $("#about").click(function(){
+        alert("alert");
+    });
+
+    $('#new').click(function(reloadPage){
+        window.location.reload();
+    })
+}
+
+function letterMatched (){
+    for(i = 0; i < arrRandomWord.length; i++){
+        if(letterSelected === arrRandomWord[i]){
+            spaces[i] = letterSelected;
+            $('#wordPlayed').text(spaces.join(' '));
+        }
+    }
+    if(spaces.indexOf('_ ') === -1){
+        $('#tries').html('You win');
+        $('.letters').off('click');
+    }
+}
+
+function decrementTries(){
+     if(randomWord.indexOf(letterSelected) === -1){
+         triesLeft -=1;
+        if(triesLeft === 0){
+            $('#tries').html('Gameover');
+            $('.letters').off('click');
+            $('.wordplay_wrapper').empty('spaces');
+            alert(arrRandomWord.join(''));
+        } else {
+            $('#tries').html('Tries Left '+ triesLeft)
+        }
+    }
+}
+
+function disableLetter(event){
+    $(event.currentTarget).addClass('active')
+    $(event.currentTarget).off('click');
+}
+
+
+
+init();
