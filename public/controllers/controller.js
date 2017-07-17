db6f90259968d50568d046fbf4c550efa1996161
JaceGarcia <jaces.place@gmail.com>
2017-07-17 10:16:51 -0400

    added to the about button

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -57,7 +57,7 @@ function createListeners(){
     });
 
     $("#about").click(function(){
-        alert("alert");
+        alert("This is a game of hangman and the goal is to guess the country before you fun out of tries. ");
     });
 
     $('#new').click(function(reloadPage){
