7e3c6abc69ae11f3cf9a4d7fc3c4ac86c28ba51c
JaceGarcia <jaces.place@gmail.com>
2017-07-17 08:50:10 -0400

    added a working hang man

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,39 +1,16 @@
 
 var alphabet = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 var words = ['afghanistan', 'albania', 'brazil', 'botswana', 'canada', 'cuba', 'denmark', 'dominica', 'egypt', 'ethiopia', 'fiji', 'finland', 'france', 'gabon', 'greece', 'hati', 'honduras', 'india', 'iran', 'japan', 'jordan', 'kenya', 'kuuait', 'libya'];
+var arrBodyLines = ['leftLeg', 'rightLeg', 'rightArm', 'leftArm','Torso', 'Head'];
 var randomNumber = Math.floor(words.length * Math.random());
 var randomWord = words[randomNumber];
 var arrRandomWord = randomWord.split('');
-var triesLeft = 6;
+//var triesLeft = 6;
 var letterSelected;
 var i;
 var spaces = [];
 var canvas = document.querySelector("canvas");
-console.log(canvas);
 var c = canvas.getContext('2d');
-
-//Hangman frame
-
-c.beginPath();
-c.moveTo(240, 260);
-c.lineTo(350, 260);
-c.stroke();
-c.moveTo(300, 20);
-c.lineTo(300, 260);
-c.stroke();
-c.moveTo(300, 20);
-c.lineTo(215, 20);
-c.stroke();
-c.moveTo(215, 20);
-c.lineTo(215, 30);
-c.stroke();
-
-
-
-
-
-
-
 
 function init() {
     var counter = 0;
@@ -46,7 +23,7 @@ function init() {
 
     $('#wordPlayed').append(spaces);
 
-    $('#tries').html('Tries Left '+ triesLeft);
+//    $('#tries').html('Tries Left '+ triesLeft);
 
     for(i = 0; i < alphabet.length; i++) {
         $('#kb' + counter).append('<div class="btn btn-info letters ">'+alphabet[i]+'</div>');
@@ -57,6 +34,21 @@ function init() {
     }
 
     createListeners();
+
+    //Hangman frame
+    c.beginPath();
+    c.moveTo(240, 260);
+    c.lineTo(350, 260);
+    c.stroke();
+    c.moveTo(300, 20);
+    c.lineTo(300, 260);
+    c.stroke();
+    c.moveTo(300, 20);
+    c.lineTo(215, 20);
+    c.stroke();
+    c.moveTo(215, 20);
+    c.lineTo(215, 30);
+    c.stroke();
 }
 
 function createListeners(){
@@ -88,17 +80,14 @@ function letterMatched (){
         $('.letters').off('click');
     }
 }
-
 function decrementTries(){
      if(randomWord.indexOf(letterSelected) === -1){
-         triesLeft -=1;
-        if(triesLeft === 0){
+         drawLines(arrBodyLines.pop());
+        if(arrBodyLines.length === 0){
             $('#tries').html('Gameover');
             $('.letters').off('click');
             $('.wordplay_wrapper').empty('spaces');
             alert(arrRandomWord.join(''));
-        } else {
-            $('#tries').html('Tries Left '+ triesLeft)
         }
     }
 }
@@ -108,6 +97,43 @@ function disableLetter(event){
     $(event.currentTarget).off('click');
 }
 
-
+function drawLines(bp){
+    if(bp === 'Head'){
+        //Head
+        c.beginPath();
+        c.arc(215, 50, 19, 0, Math.PI * 2, false);
+        c.stroke();
+    } else if(bp === 'Torso'){
+        //Torso
+        c.beginPath();
+        c.moveTo(215, 68);
+        c.lineTo(215, 150);
+        c.stroke();
+    } else if (bp === 'leftArm'){
+        //Left arm
+        c.beginPath();
+        c.moveTo(215, 90);
+        c.lineTo(180, 130);
+        c.stroke();
+    } else if (bp ==='rightArm'){
+        //Right arm
+        c.beginPath();
+        c.moveTo(215, 90);
+        c.lineTo(250, 130);
+        c.stroke();
+    } else if (bp === 'rightLeg'){
+        // Right leg
+        c.beginPath();
+        c.moveTo(215, 150);
+        c.lineTo(250, 200);
+        c.stroke();
+    } else if (bp === 'leftLeg'){
+        // Left leg
+        c.beginPath();
+        c.moveTo(215, 150);
+        c.lineTo(180, 200);
+        c.stroke();
+    }
+}
 
 init();
